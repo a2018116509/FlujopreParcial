deaa3852737d13d2c3d09dd2c1e0eda807815998
a2018116509 <a2018116509@estudiantes.upsa.edu.bo>
2021-03-14 18:09:04 -0400

    Enviando correos

--- fncConsumidor/Function1.cs
+++ fncConsumidor/Function1.cs
@@ -1,16 +1,48 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System.Net.Mail;
+using Newtonsoft.Json;
+using fncConsumidor.Models;
 
 namespace fncConsumidor
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("ejercicios", Connection = "MyConn")]string myQueueItem, ILogger log)
+        public static void Run(
+            [ServiceBusTrigger(
+                    "ejercicios",
+                    Connection = "MyConn"
+            )] string myQueueItem,
+
+            ILogger log
+        )
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            try
+            {
+                log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+                var dataOdometer = JsonConvert.DeserializeObject<Odometro>(myQueueItem);
+                //Aqui va la accion await datos.AddAsync(data);
+                MailMessage message = new MailMessage();
+                 message.To.Add(dataOdometer.Email); //Email from queue
+                message.Subject = "YourOdometer";
+                message.From = new MailAddress("mariana.arnez@outlook.es"); //My Email
+                message.Body = $"Hola! {dataOdometer.Name}, desde la última toma, caminaste {dataOdometer.Step} pasos.";
+
+                SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com");
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("mariana.arnez@outlook.es", "abril,2003");
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"It is not possible to send mails: {ex.Message}");
+            }
         }
+
+
     }
 }
--- fncConsumidor/Models/Odometro.cs
+++ fncConsumidor/Models/Odometro.cs
@@ -1,4 +1,4 @@
-﻿namespace ApiColector.Models
+﻿namespace fncConsumidor.Models
 {
     using System;
     using System.ComponentModel.DataAnnotations;
